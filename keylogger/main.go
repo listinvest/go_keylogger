51c08a0bd999530d26a48f2fce0dfec72ac4a151
Ryan Fredrick <38120663+reinoj@users.noreply.github.com>
2020-07-22 00:40:35 -0500

    added left and right alt codes and commented out main part and put in test stuff again

--- keylogger/main.go
+++ keylogger/main.go
@@ -18,37 +18,44 @@ func main() {
 	time.Sleep(2 * time.Second)
 
 	// stores active keys from most recent cycle
-	var CurrentlyActiveKeys [103]bool
+	// var CurrentlyActiveKeys [103]bool
 	for c := 0; c < 500; c++ {
-		// start := time.Now()
-		for _, key := range keyboard.Keys {
-			// ret is a short where if the most or least significant bits are set then the key is being pressed or was unpressed since the last check
-			ret, _, _ := getAsyncKeyState.Call(uintptr(key))
+		// 	// start := time.Now()
+		// 	for _, key := range keyboard.Keys {
+		// 		// ret is a short where if the most or least significant bits are set then the key is being pressed or was unpressed since the last check
+		// 		ret, _, _ := getAsyncKeyState.Call(uintptr(key))
+		// 		if uint16(ret) != 0 {
+		// 			CurrentlyActiveKeys[keyboard.KeyIndex[key]] = true
+		// 		} else {
+		// 			CurrentlyActiveKeys[keyboard.KeyIndex[key]] = false
+		// 		}
+		// 	}
+		// 	// go through each key's bool value and set/unset the main bool array and increment the times pressed counter
+		// 	for i := 0; i < keyboard.NumKeys; i++ {
+		// 		if CurrentlyActiveKeys[i] && !keyboard.IsKeyActive[i] {
+		// 			keyboard.IsKeyActive[i] = true
+		// 		} else if !CurrentlyActiveKeys[i] && keyboard.IsKeyActive[i] {
+		// 			keyboard.IsKeyActive[i] = false
+		// 			keyboard.TimesPressed[i]++
+		// 		}
+		// 	}
+		// 	// total := time.Since(start)
+		// 	// fmt.Println(total)
+
+		// 	// doesn't need to be running constantly
+		// 	time.Sleep(10 * time.Millisecond)
+		// 	// break
+		// }
+		for i := 0; i < 256; i++ {
+			ret, _, _ := getAsyncKeyState.Call(uintptr(i))
 			if uint16(ret) != 0 {
-				CurrentlyActiveKeys[keyboard.KeyIndex[key]] = true
-			} else {
-				CurrentlyActiveKeys[keyboard.KeyIndex[key]] = false
+				fmt.Printf("%#X\n", i)
 			}
 		}
-
-		// go through each key's bool value and set/unset the main bool array and increment the times pressed counter
-		for i := 0; i < keyboard.NumKeys; i++ {
-			if CurrentlyActiveKeys[i] && !keyboard.IsKeyActive[i] {
-				keyboard.IsKeyActive[i] = true
-			} else if !CurrentlyActiveKeys[i] && keyboard.IsKeyActive[i] {
-				keyboard.IsKeyActive[i] = false
-				keyboard.TimesPressed[i]++
-			}
-		}
-		// total := time.Since(start)
-		// fmt.Println(total)
-
-		// doesn't need to be running constantly
 		time.Sleep(10 * time.Millisecond)
-		// break
 	}
 
 	for i, tp := range keyboard.TimesPressed {
-		fmt.Printf("%#x: %d\n", keyboard.VirtualKeyCodes[keyboard.Keys[i]], tp)
+		fmt.Printf("%s: %d\n", keyboard.VirtualKeyCodes[keyboard.Keys[i]], tp)
 	}
 }
